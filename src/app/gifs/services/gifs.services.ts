a34e5bcf0e611745f44559042196adb663315ab0
carloscoronaba <carlos.coronaba@gmail.com>
2024-07-31 13:56:22 -0600

    gifs con localStorage

--- src/app/gifs/services/gifs.services.ts
+++ src/app/gifs/services/gifs.services.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class GifsService {
 
   public gifList: Gif[] = [];
@@ -11,36 +11,53 @@ export class GifsService {
   private apiKey: string = 'zj3XmW9hgAN2j0G53urYoLgTl8ZdZIvE'
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+    // console.log('Gifs service initialized')
+  }
 
-  get tagsHistory(){
+  get tagsHistory() {
     return [...this._tagsHistory];
   }
 
-  private organizerHistory(tag:string){
+  private organizerHistory(tag: string) {
     tag = tag.toLowerCase();
-    if(this._tagsHistory.includes(tag)){
+    if (this._tagsHistory.includes(tag)) {
       this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag)
     }
 
     this._tagsHistory.unshift(tag);
-    this._tagsHistory = this._tagsHistory.splice(0,10);
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
   }
 
-  searchTag(tag: string):void {
-    if(tag.length === 0) return;
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory))
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if(this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0]);
+  }
+
+  searchTag(tag: string): void {
+    if (tag.length === 0) return;
     this.organizerHistory(tag);
 
     const params = new HttpParams()
-      .set('api_key',this.apiKey)
-      .set('limit',10)
-      .set('q',tag)
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag)
 
-    this.http.get<SearchResponse>(`${this.serviceUrl}/search?`,{params})
-    .subscribe(resp =>{
-      // console.log(resp);
-      this.gifList = resp.data;
-      // console.log({gifs:this.gifList})
-    });
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search?`, { params })
+      .subscribe(resp => {
+        // console.log(resp);
+        this.gifList = resp.data;
+        // console.log({gifs:this.gifList})
+      });
   }
 }
